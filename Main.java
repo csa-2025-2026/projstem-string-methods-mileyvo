75b57b54b1c03092586ac5d694787e1de9885105
mileyvo <mtvo106@student.hbuhsd.edu>
2025-09-29 18:22:05 +0000

    09/29/2025

--- Main.java
+++ Main.java
@@ -3,6 +3,10 @@ public class Main
   public static void main(String[] args)
   {
     // write your code here
+    
+    
+    
+    lastFirstN("cream", "butter", 3);
   }
 
   /** Precondition: s1 and s2 are not null
@@ -10,14 +14,41 @@ public class Main
   */
   public static void lastFirstN(String s1, String s2, int n)
   {
-  
+    String output;
+    String firstN;
+    String lastN;
+
+    firstN = s2.substring(0, n);
+    int startPosition = s1.length() - n;
+    lastN = s1.substring(n);
+
+
+    output = lastN + firstN;
+    System.out.println(output);
+
   }
 
   /** Precondition: s1 and s2 are not null
   */
   public static void stringManip(String s1, String s2)
   {
-  
+    String firstS;
+    firstS = s1.toUpperCase();
+    System.out.println(firstS);
+
+    String secondS;
+    String firstL;
+    String restL;
+
+    firstL = s2.substring(0,1);
+    firstL = firstL.toUpperCase();
+
+    restL = s2.substring(1);
+    restL = restL.toLowerCase();
+
+    secondS = firstL + restL;
+    System.out.println(secondS);
+
   }
 
   /** Precondition: s1 and s2 are not null
